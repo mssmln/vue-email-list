214cee69419a7228e720c84ad2be95f38f9af717
Massimiliano <m.croci.ly@gmail.com>
2021-02-08 15:09:24 +0100

    trying with jquery

--- js/script.js
+++ js/script.js
@@ -2,20 +2,35 @@
 // Potete utilizzare questa api:
 // https://flynn.boolean.careers/exercises/api/random/mail
 
-var app = new Vue ({
-  el: '#app',
-  data: {
-    mails: [] // inizializzato vuoto per le mails che verranno generate
-  },
-  mounted(){
-    for (let i = 0; i < 10; i++){
-      axios
-      .get('https://flynn.boolean.careers/exercises/api/random/mail')
-      .then((result) => {
-        console.log(result.data);
-        this.mails.push(result.data.response);
-      });
+// // opzione con Vue
+// var app = new Vue ({
+//   el: '#app',
+//   data: {
+//     mails: [] // inizializzato vuoto per le mails che verranno generate
+//   },
+//   mounted(){
+//     for (let i = 0; i < 10; i++){
+//       axios
+//       .get('https://flynn.boolean.careers/exercises/api/random/mail')
+//       .then((result) => {
+//         console.log(result.data);
+//         this.mails.push(result.data.response);
+//       });
+//
+//     }
+//   }
+// });
 
+
+// opzione con jQuery
+$(document).ready(function() {
+  $.ajax(
+    {
+      url: "https://flynn.boolean.careers/exercises/api/random/mail",
+      method: "GET",
+      success: function(data) {
+        $("#generatedMail").html(data);
+      },
     }
-  }
+  );
 });
