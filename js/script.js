b6ba0a1f3194176837d68616144b5d933683780d
Massimiliano <m.croci.ly@gmail.com>
2021-02-08 16:19:41 +0100

    made the Vue version one the main

--- js/script.js
+++ js/script.js
@@ -2,24 +2,24 @@
 // Potete utilizzare questa api:
 // https://flynn.boolean.careers/exercises/api/random/mail
 
-// // opzione con Vue
-// var app = new Vue ({
-//   el: '#app',
-//   data: {
-//     mails: [] // inizializzato vuoto per le mails che verranno generate
-//   },
-//   mounted(){
-//     for (let i = 0; i < 10; i++){
-//       axios
-//       .get('https://flynn.boolean.careers/exercises/api/random/mail')
-//       .then((result) => {
-//         console.log(result.data);
-//         this.mails.push(result.data.response);
-//       });
-//
-//     }
-//   }
-// });
+// opzione con Vue
+var app = new Vue ({
+  el: '#app',
+  data: {
+    mails: [] // inizializzato vuoto per le mails che verranno generate
+  },
+  mounted(){
+    for (let i = 0; i < 10; i++){
+      axios
+      .get('https://flynn.boolean.careers/exercises/api/random/mail')
+      .then((result) => {
+        console.log(result.data);
+        this.mails.push(result.data.response);
+      });
+
+    }
+  }
+});
 
 
 // // opzione con jQuery
@@ -42,3 +42,18 @@
 //   console.log(mails);
 //
 // });
+
+
+// opzione con js vanilla
+// const mails = [];
+//
+// for (let i = 0; i < 10; i++){
+//   fetch('https://flynn.boolean.careers/exercises/api/random/mail')
+//   .then(response => response.json()) // crea il file json
+//   .then(response => {
+//     // console.log(response.response)
+//     mails.push(response.response);
+//     document.getElementById('generatedMail').innerHTML = mails;
+//   });
+// }
+// console.log(mails);
